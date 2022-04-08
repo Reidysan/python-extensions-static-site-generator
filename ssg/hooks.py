7b88ce5c1a37a34da1aaba59c9bedbd4e52903d3
unknown <callum.reid92@hotmail.com>
2022-04-08 14:38:42 +1000

    Completed module

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -1,17 +1,17 @@
-_callback = {}
+_callbacks = {}
 
 def register(hook, order: 0):
     def event(self,hook, *args):
-        for order in sorted(_callback.get(hook, {})):
-            for func in self._callbacks[hook][order]:
+        for order in sorted(_callbacks.get(hook, {})):
+            for func in _callbacks[hook][order]:
                 func(*args)
 
-    def filter(self, hook, value, *args):
-        for order in sorted(_callback.get(hook, {})):
-            for func in self._callbacks[hook][order]:
+    def filter(hook, value, *args):
+        for order in sorted(_callbacks.get(hook, {})):
+            for func in _callbacks[hook][order]:
                 func(value, *args)
         return value 
     def register_callback(func):
-        _callback.setdefault(hook, {}, order, [].append(func))
+        _callbacks.setdefault(hook, {}, order, [].append(func))
         return func
     return register_callback
