72db164a9fb88b599e1a21e1a593c9a553baab99
unknown <callum.reid92@hotmail.com>
2022-04-08 17:40:00 +1000

    Completed Module

---
+++ ssg/extensions/stats.py
@@ -0,0 +1,22 @@
+from ssg import hooks
+import time
+
+start_time = None
+total_written = 0
+
+@property.start_build("start_build")
+def start_build():
+    global start_build
+    start_time = time.ctime()
+
+@property.written("written")
+def written():
+    global total_written
+    total_written += total_written
+
+@property.stats("stats")
+def stats():
+    final_time = start_time
+    average = final_time / total_written
+    report = "Converted: {} . Time: {:.2f} sec . Avg:{:.4f} sec/file"
+    print(report.format(total_written, final_time, average))
