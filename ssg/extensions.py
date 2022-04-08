05a420e1cf277905a1de884b69bcaebd75e0f11c
unknown <callum.reid92@hotmail.com>
2022-04-08 11:55:55 +1000

    Completed module

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -8,8 +8,7 @@ def load_module(directory, name):
     sys.path.pop(0)
 
 def load_directory(directory):
-    for path in directory:
-        Path.rglob(".py")
+    for path in directory.rglob(".py"):
         load_module(directory.as_posix(path.stem))
 
 def loaded_bundled(self):
