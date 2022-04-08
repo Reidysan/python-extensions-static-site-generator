62cf935098b88fad192ae56650608e8f866d5478
unknown <callum.reid92@hotmail.com>
2022-04-08 11:54:37 +1000

    Completed module

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -1,12 +1,11 @@
-from msilib.schema import Directory
 import sys
 import importlib
 from pathlib import Path
 
 def load_module(directory, name):
-    sys.path.insert(directory[0])
+    sys.path.insert(0, directory)
     importlib.import_module(name)
-    sys.path.pop([0])
+    sys.path.pop(0)
 
 def load_directory(directory):
     for path in directory:
